a18d9e9926053cfc1d41ec32ed3de0b7a2c76df3
jeetendra mittal <jeetu.cs23@gmail.com>
2023-02-24 00:40:17 +0530

    hello

--- test/src/test/java/test/MyClass.java
+++ test/src/test/java/test/MyClass.java
@@ -16,7 +16,7 @@ public class MyClass {
 		System.out.println("hello jenkins pipeline");
 		System.out.println("hello jenkins pipeline");
 
-		
+		System.out.println("hello jenkins pipeline");
 	}
 
 }
